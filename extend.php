5c3eb15cbc6f3b3b0d6bb853e60d23659d1f7086
Zephyr-Isle <zephyr_isle@outlook.com>
2026-05-03 22:41:18 +0800

    修正 Flarum v2 主题 API

--- extend.php
+++ extend.php
@@ -1,9 +1,12 @@
 <?php
 
 use Flarum\Extend;
-use Flarum\Extend\Theme;
+use Flarum\Extend\Frontend;
 
 return [
-    (new Theme)
-        ->addCSS(__DIR__.'/resources/less/forum.less'),
+    (new Frontend('forum'))
+        ->css(__DIR__.'/resources/less/forum.less'),
+
+    (new Frontend('admin'))
+        ->css(__DIR__.'/resources/less/forum.less'),
 ];
